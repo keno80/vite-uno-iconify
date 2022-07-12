32aeec4f6954d44c4b4b3ba7086a381724de8ca4
keno80 <1211582428@qq.com>
2022-07-12 18:42:06 +0800

    feat: 添加router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import 'uno.css'
 import './styles/app.scss'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from '@/components/Layout/Header.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('@/views/Home/index.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(),
+})
+
+export default router
